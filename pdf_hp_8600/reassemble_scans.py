a92e5d365438524824c16b682a15a7d592a231ad
tim <tim@sy-edm.com>
2018-06-04 17:12:26 +0400

    Tests now added.

--- pdf_hp_8600/reassemble_scans.py
+++ pdf_hp_8600/reassemble_scans.py
@@ -1,13 +1,22 @@
+from glob import glob
 import PyPDF2
 import daiquiri
-from glob import glob
+
 
 class reassemble_scans(object):
+    """
+    Class to reassemble a multipage scan document
+    Useage:
+    rs = reassemble_scans("Directory_where_2_pdfs_live")
+    rs.find_finds()
+    rs.reassemble("/tmp/new_document.pdf")
+    """
 
     def __init__(self,directory_for_scans):
         self.dir_to_search=directory_for_scans
         self.logging = daiquiri.getLogger(__name__)
 
+    @staticmethod
     def help(self):
         message=\
 """
@@ -32,26 +41,34 @@ The first will have pages (1,3,5,7,9) on it.... then 2nd will have pages (10,8,6
 This script will make the new_document have pages in the order of (1,2,3,4,....)
 
 """
-
         print(""+message)
 
 
     def find_finds(self,dir=None):
+        """
+
+        :param dir:
+        :return: Length of the number of files found in the directory
+        """
         if dir==None:
-            where=dir
-        else:
             where=self.dir_to_search
-        self.files = glob(dir+"/*.pdf")
+        else:
+            where=dir
+        self.files = glob(where+"/*.pdf")
 
         for f in self.files:
             print("{}".format(f))
             self.logging.info("Files are {}".format(f))
+        return len(self.files)
 
     def check_2_scans(self):
+        """
+        There should only be 2 pdf documents in the target directory
+        :return:
+        """
         if len(self.files) == 2:
             self.logging.debug("Correct number of files found")
             return True
-
 
     def reassemble(self,new_file_name):
         """
@@ -60,24 +77,29 @@ This script will make the new_document have pages in the order of (1,2,3,4,....)
         :return:  True if Ok - ValueError if the {ageCount is different
 
         """
-        f1 = PyPDF2.PdfFileReader(open(self.files[0], 'rb'))
-        f2 = PyPDF2.PdfFileReader(open(self.files[1], 'rb'))
+        try:
 
-        self.logging.debug("F1 Has {} Pages".format(f1.getNumPages()))
-        self.logging.debug("F2 Has {} Pages".format(f2.getNumPages()))
+            f1 = PyPDF2.PdfFileReader(open(self.files[0], 'rb'))
+            f2 = PyPDF2.PdfFileReader(open(self.files[1], 'rb'))
 
-        if f1.getNumPages() == f2.getNumPages():
-            f2_next_page = f2.getNumPages() - 1
-            f1_next_page = 1
+            self.logging.debug("F1 Has {} Pages".format(f1.getNumPages()))
+            self.logging.debug("F2 Has {} Pages".format(f2.getNumPages()))
 
-            new_doc = PyPDF2.PdfFileWriter()
-            while (f1_next_page < f1.getNumPages()):
-                new_doc.addPage(f1.getPage(f1_next_page))
-                new_doc.addPage(f2.getPage(f2_next_page))
-                self.logging.info("Page {} and {} added ".format(f1_next_page, f2_next_page))
-                f1_next_page += 1
-                f2_next_page -= 1
-            new_doc.write(open(new_file_name, "wb"))
+            if f1.getNumPages() == f2.getNumPages():
+                f2_next_page = f2.getNumPages() - 1
+                f1_next_page = 1
 
-        else:
-            raise ValueError
+                new_doc = PyPDF2.PdfFileWriter()
+                while f1_next_page < f1.getNumPages():
+                    new_doc.addPage(f1.getPage(f1_next_page))
+                    new_doc.addPage(f2.getPage(f2_next_page))
+                    self.logging.info("Page {} and {} added ".format(f1_next_page, f2_next_page))
+                    f1_next_page += 1
+                    f2_next_page -= 1
+                new_doc.write(open(new_file_name, "wb"))
+                return True
+            else:
+                return False
+        except Exception as err:
+            self.logging("Error {} ".format(str(err)))
+            return False
--- pdf_hp_8600/tests/test_01.py
+++ pdf_hp_8600/tests/test_01.py
@@ -1,16 +1,32 @@
 from pdf_hp_8600 import reassemble_scans
 
+from inspect import getsourcefile
+from os import chdir, unlink
+from os.path import abspath, dirname,exists
+
+
 class Tester(object):
 
     def __call__(self, value, expected, message):
         self.description = 'Test {}'.format(message)
-        assert value == expected, '{} {}'.format(value,expected)
+        assert value == expected, '{} {}'.format(value, expected)
+
 
 def test_010():
     rs_obj = reassemble_scans("/users")
-    yield Tester(), rs_obj , rs_obj , "reassemble object init ok"
+    yield Tester(), rs_obj, rs_obj, "reassemble object init ok"
+
 
 def test_020():
-    rs_obj = reassemble_scans(".")
-    rs_obj.find_finds()
+    where_are_we = dirname(abspath(getsourcefile(lambda: 0)))
+    try:
+        unlink(where_are_we + "/new.pdf")
+    except Exception as err:
+        pass
+    rs_obj = reassemble_scans(where_are_we)
+    yield Tester(),  2,  rs_obj.find_finds(), "Check 2 files"
     yield Tester(), True, rs_obj.check_2_scans(), "Check 2 files in directory"
+    rs_obj.reassemble(where_are_we+"/new.pdf")
+    yield Tester(), True, exists(where_are_we+"/new.pdf"), "New pdf Created"
+
+
